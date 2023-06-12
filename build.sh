57064d4188206717001dbad696116918b64190ed
athlon18 <44226614+athlon18@users.noreply.github.com>
2023-06-12 14:16:09 +0800

    update dockerfile to k8s

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# npm run build
+docker-compose build --force-rm
+docker build -t 192.168.1.220:5000/hopeshow_wcsflow:latest .
+docker push 192.168.1.220:5000/hopeshow_wcsflow:latest
+curl -X PUT \
+    -H "Content-Type: application/yaml" \
+    -H "Cookie: KuboardUsername=admin; KuboardAccessKey=rhyjdskab6z6.e36pbkd7w4nwis3n3jkxjpmk2tj5zrjz" \
+    -d '{"kind":"deployments","namespace":"wcsflow","name":"wcsflow"}' \
+    "http://192.168.1.220:81/kuboard-api/cluster/hopeshow/kind/CICDApi/admin/resource/restartWorkload"
